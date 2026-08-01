cece956710b8e43ca2f52a5c6a8b4686be43e567
dmvled-coder <dmvled@gmail.com>
2026-08-01 22:03:54 +0700

    sửa chiều cao radar

--- game.js
+++ game.js
@@ -87,7 +87,7 @@
 
     function forceResize() {
         canvas.width = window.innerWidth; canvas.height = window.innerHeight;
-        cfg.gap = canvas.width > 800 ? 120 : 140;
+        cfg.gap = canvas.width > 800 ? 120 : 110;
         plane.w = canvas.width > 800 ? 90 : 70;
         plane.h = canvas.width > 800 ? 40 : 30;
         plane.x = canvas.width / 5;
