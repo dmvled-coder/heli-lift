46cb1395a5b96ac20fbfcf2e41e707c07d5676e0
dmvled-coder <dmvled@gmail.com>
2026-08-06 21:11:28 +0700

    Tạo hàm cặp mã hóa gửi điểm

---
+++ Servicefuntion.js
@@ -0,0 +1,9 @@
+export function endgameservice() {
+    const min = -10;
+    const max = 20;
+    const b1 = Math.random() * (max - min) + min;
+  console.log("b1 =", result.b1.toFixed(2));
+    const a1 = Math.sin(b1) + 1;
+
+    return { b1, a1 };
+}
