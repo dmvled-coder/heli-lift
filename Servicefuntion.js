d79b0fa0811a231c70bbaeac11a6476304ca2854
dmvled-coder <dmvled@gmail.com>
2026-08-06 22:33:20 +0700

    Refactor endgameservice function definition

--- Servicefuntion.js
+++ Servicefuntion.js
@@ -1,9 +1,8 @@
-export function endgameservice() {
+// 🟢 CODE ĐÃ SỬA CHUẨN:
+function endgameservice() {
     const min = -10;
     const max = 20;
     const b1 = Math.random() * (max - min) + min;
-  
     const a1 = Math.sin(b1) + 1;
-
     return { b1, a1 };
 }
