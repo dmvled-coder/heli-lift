80a959b6f4fcf206646dff7094fc3ef31222feae
dmvled-coder <dmvled@gmail.com>
2026-08-13 19:01:16 +0700

    raddar còn 50 màn hình nga trên 800

--- game.js
+++ game.js
@@ -87,7 +87,7 @@
 
     function forceResize() {
         canvas.width = window.innerWidth; canvas.height = window.innerHeight;
-        cfg.gap = canvas.width > 800 ? 105 : 100;
+        cfg.gap = canvas.width > 800 ? 50 : 100;
         plane.w = canvas.width > 800 ? 90 : 70;
         plane.h = canvas.width > 800 ? 40 : 30;
         plane.x = canvas.width / 5;
